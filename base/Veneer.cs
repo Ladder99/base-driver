12859194171ea54dc96795ab4fce37a04f99fe97
MRIIOT <cmisztur@mriiot.com>
2023-03-25 15:55:46 -0500

    * add previous observation to veneer

--- base/Veneer.cs
+++ base/Veneer.cs
@@ -5,20 +5,40 @@ namespace l99.driver.@base;
 
 public class Veneer
 {
-    private readonly Stopwatch _stopwatchDataArrival = new();
-
-    private readonly Stopwatch _stopwatchDataChange = new();
     protected readonly ILogger Logger;
-
+    
     protected readonly Veneers Veneers;
 
     private bool _isFirstCall = true;
 
     public Func<Veneer, Task> OnArrivalAsync = async veneer => { await Task.FromResult(0); };
-
     public Func<Veneer, Task> OnChangeAsync = async veneer => { await Task.FromResult(0); };
-
     public Func<Veneer, Task> OnErrorAsync = async veneer => { await Task.FromResult(0); };
+
+    public string Name { get; }
+    public bool IsInternal { get; }
+    public bool IsCompound { get; }
+    public dynamic SliceKey { get; private set; } = null!;
+    public IEnumerable<dynamic> Marker { get; private set; } = null!;
+    public bool HasMarker { get; private set; }
+
+    private readonly Stopwatch _stopwatchDataArrival = new();
+    private readonly Stopwatch _stopwatchDataChange = new();
+    
+    public TimeSpan ArrivalDelta => _stopwatchDataArrival.Elapsed;
+    public TimeSpan ChangeDelta => _stopwatchDataChange.Elapsed;
+
+    protected dynamic[] LastArrivedNativeInputs { get; private set; } = null!;
+    protected dynamic[] LastArrivedAdditionalInputs { get; private set; } = null!;
+    public dynamic LastArrivedValue { get; private set; } = null!;
+
+    protected dynamic[] PreviouslyChangedNativeInputs { get; private set; } = null!;
+    protected dynamic[] PreviouslyChangedAdditionalInputs { get; private set; } = null!;
+    public dynamic PreviouslyChangedValue { get; private set; } = null!;
+    
+    protected dynamic[] LastChangedNativeInputs { get; private set; } = null!;
+    protected dynamic[] LastChangedAdditionalInputs { get; private set; } = null!;
+    public dynamic LastChangedValue { get; protected set; } = null!;
 
     protected Veneer(Veneers veneers, string name = "", bool isCompound = false, bool isInternal = false)
     {
@@ -29,35 +49,7 @@ public class Veneer
         IsInternal = isInternal;
         _stopwatchDataChange.Start();
     }
-
-    public string Name { get; }
-
-    public bool IsInternal { get; }
-
-    public bool IsCompound { get; }
-
-    public dynamic SliceKey { get; private set; } = null!;
-
-    public IEnumerable<dynamic> Marker { get; private set; } = null!;
-
-    public bool HasMarker { get; private set; }
-
-    public TimeSpan ArrivalDelta => _stopwatchDataArrival.Elapsed;
-
-    public TimeSpan ChangeDelta => _stopwatchDataChange.Elapsed;
-
-    protected dynamic[] LastArrivedNativeInputs { get; private set; } = null!;
-
-    protected dynamic[] LastArrivedAdditionalInputs { get; private set; } = null!;
-
-    protected dynamic[] LastChangedNativeInputs { get; private set; } = null!;
-
-    protected dynamic[] LastChangedAdditionalInputs { get; private set; } = null!;
-
-    public dynamic LastArrivedValue { get; private set; } = null!;
-
-    public dynamic LastChangedValue { get; protected set; } = null!;
-
+    
     protected async Task OnDataArrivedAsync(dynamic[] nativeInputs, dynamic[] additionalInputs, dynamic currentValue)
     {
         Logger.Trace($"[{Name}] Veneer arrival invocation result:\n{JObject.FromObject(currentValue).ToString()}");
@@ -71,9 +63,15 @@ public class Veneer
     protected async Task OnDataChangedAsync(dynamic[] nativeInputs, dynamic[] additionalInputs, dynamic currentValue)
     {
         Logger.Trace($"[{Name}] Veneer change invocation result:\n{JObject.FromObject(currentValue).ToString()}");
+        
+        // move last to previous and incoming to last
+        PreviouslyChangedNativeInputs = LastChangedNativeInputs;
         LastChangedNativeInputs = nativeInputs;
+        PreviouslyChangedAdditionalInputs = LastArrivedAdditionalInputs;
         LastChangedAdditionalInputs = additionalInputs;
+        PreviouslyChangedValue = LastChangedValue;
         LastChangedValue = currentValue;
+        
         await OnChangeAsync(this);
         _stopwatchDataChange.Restart();
     }
